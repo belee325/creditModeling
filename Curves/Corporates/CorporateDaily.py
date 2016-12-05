8fefd312c67a00d2bf5c242d37e6ffb8f1c5cd78
belee325 <belee325@gmail.com>
2016-12-04 22:13:46 -0500

    q bootstrapped

--- Curves/Corporates/CorporateDaily.py
+++ Curves/Corporates/CorporateDaily.py
@@ -5,21 +5,35 @@ import quandl
 import pickle, os
 from Scheduler.Scheduler import Scheduler
 from parameters import WORKING_DIR
+from MonteCarloSimulators.Vasicek.vasicekMCSim import MC_Vasicek_Sim
+from fredapi import Fred
 
+QUANDL_API_KEY = "i1GzZpZsfBpPpazzR4Mg"
+FRED_API_KEY = "Create Your Own"
 
 class CorporateRates(object):
     def __init__(self):
         self.OIS = []
         self.filename = WORKING_DIR + '/CorpData.dat'
-        #self.corporates = []
-        self.ratings = ['AAA', 'AA', 'A', 'BBB', 'BB', 'B', 'CCC']
+        self.corporates = []
+        self.ratings = {'AAA':"BAMLC0A1CAAA",
+                       'AA':"BAMLC0A2CAA",
+                       'A':"BAMLC0A3CA",
+                       'BBB':"BAMLC0A4CBBB",
+                       'BB':"BAMLH0A1HYBB",
+                        'B':"BAMLH0A2HYB",
+                        'CCC':"BAMLH0A3HYC"}
         self.corpSpreads = {}
         self.corporates = pd.DataFrame()
+        self.Qcorporates = pd.DataFrame() # survival function for corporates
         self.tenors = []
         self.unPickleMe(file=self.filename)
         self.myScheduler=Scheduler()
+        self.myVasicek = MC_Vasicek_Sim()
+        self.R = 0.4
 
-    def getCorporates(self, trim_start, trim_end):
+    def getCorporatesFred(self, trim_start, trim_end):
+        fred = Fred(api_key=FRED_API_KEY)
         curr_trim_end=trim_start
         if(self.corporates.size!=0):
             self.trim_start = self.corporates['OIS'].index.min().date()
@@ -32,20 +46,22 @@ class CorporateRates(object):
         self.OIS = OIS(trim_start=trim_start, trim_end=trim_end)
         self.datesAll = self.OIS.datesAll
         self.datesAll.columns= [x.upper() for x in self.datesAll.columns]
+        self.datesAll.index = self.datesAll.DATE
         self.OISData = self.OIS.getOIS()
         for i in np.arange(len(self.OISData.columns)):
             freq = self.OISData.columns[i]
             self.tenors.append(self.myScheduler.extractDelay(freq=freq))
-        for rating in self.ratings:
-            index = 'ML/' + rating + 'TRI'
+        for rating in self.ratings.keys():
+            index = self.ratings[rating]
             try:
-                corpSpreads = 1e-4 * (
-                    quandl.get(index, authtoken="Lqsxas8ieaKqpztgYHxk", trim_start=trim_start, trim_end=trim_end))
-                corpSpreads.reset_index(level=0, inplace=True)
-                corpSpreads = pd.merge(left=self.datesAll, right=corpSpreads, how='left')
+                corpSpreads = 1e-2*(fred.get_series(index,observation_start=trim_start, observation_end=trim_end).to_frame())
+                corpSpreads.index = [x.date() for x in corpSpreads.index[:]]
+                corpSpreads = pd.merge(left=self.datesAll, right=corpSpreads, left_index=True, right_index=True, how="left")
                 corpSpreads = corpSpreads.fillna(method='ffill').fillna(method='bfill')
+                corpSpreads = corpSpreads.drop("DATE", axis=1)
                 self.corpSpreads[rating] = corpSpreads.T.fillna(method='ffill').fillna(method='bfill').T
-            except:
+            except Exception as e:
+                print(e)
                 print(index, " not found")
         self.corpSpreads = pd.Panel.from_dict(self.corpSpreads)
         self.corporates = {}
@@ -53,7 +69,7 @@ class CorporateRates(object):
         ntenors = np.shape(self.OISData)[1]
         for rating in self.ratings:
             try:
-                tiledCorps = np.tile(self.corpSpreads[rating]['VALUE'], ntenors).reshape(np.shape(self.OISData))
+                tiledCorps = np.tile(self.corpSpreads[rating][0], ntenors).reshape(np.shape(self.OISData))
                 self.corporates[rating] = pd.DataFrame(data=(tiledCorps + self.OISData.values),
                                                        index=self.OISData.index, columns=self.OISData.columns)
             except:
@@ -62,18 +78,53 @@ class CorporateRates(object):
         self.corporates = pd.Panel(self.corporates)
         return self.corporates
 
-    def getOISData(self, datelist=[]):
-        if (len(datelist) != 0):
-            return self.corporates["OIS"].loc[datelist]
-        else:
-            return self.self.corporates["OIS"]
 
+    def getCorporateData(self, rating, datelist=None):
+    # This method gets a curve for a given date or date list for a given rating (normally this will be just a date).
+    # It returns a dict of curves read directly from the corporate rates created by getCorporatesFred.
+    # Derive delays from self.corporates[rating].columns
+        myDelays = self.myScheduler.extractDelay(freq=list(self.corporates[rating].columns))
+        if datelist is None:
+            return
+        outCurve = {}
+        for day in datelist:
+        # Create curves
+        # ..............
+        # ..............
+        # add curve to outcurve dict
+            myCurve = self.corporates.loc[rating, datelist]
+            outCurve[day]=myCurve
+        return outCurve
 
-    def getCorporateData(self, rating, datelist=[]):
-        if (len(datelist) != 0):
-            return self.corporates[rating].loc[datelist]
-        else:
-            return self.corporates[rating]
+    def getCorporateQData(self, rating, datelist=None, R=0.4):
+        self.R = R
+        self.OIS = OIS()
+        outCurve = {}
+        if datelist is None:
+            return
+        myCurve = np.ones(11)
+        outCurve[datelist[0]] = myCurve
+        z = self.OIS.getOIS(datelist)
+        spread = self.getCorporateData(rating, datelist)
+        delta = (datelist[1] - datelist[0]).days/365
+        interimSum = np.zeros(11)
+        for day in datelist[1:]:
+        # Create Q curves using q-tilde equation
+            endDate = datelist.slice_indexer(start=day).start
+            for day2 in datelist[endDate-1:endDate]:
+                if day2 == datelist[0]:
+                    qPrev=np.ones(11)
+                else:
+                    qPrev=outCurve[day2-1]
+                qCurr = outCurve[day2]
+                spread = self.getCorporateData(rating, pd.date_range(start=day, end=day))
+                interimSum = interimSum + z.loc[day2].values[1:]*(outCurve[day2]*delta*spread[day].values-(1-R)*(qPrev - qCurr))
+            myCurve = (z.loc[day].values[1:]*(1-R)*outCurve[day-1] - interimSum) / (z.loc[day].values[1:]*(delta*spread[day]+1-R))
+            outCurve[day]=myCurve.values
+            #interimSum=np.zeros(11)
+            #print(day)
+        return outCurve
+
 
     def pickleMe(self):
         data = [self.corporates, self.corpSpreads]
@@ -100,8 +151,8 @@ class CorporateRates(object):
 
 # Class OIS
 class OIS(object):
-    def __init__(self, trim_start="2005-01-10", trim_end="2010-01-10"):
-        self.OIS = 0.01 * quandl.get("USTREASURY/YIELD", authtoken="Lqsxas8ieaKqpztgYHxk", trim_start=trim_start,
+    def __init__(self, trim_start="2000-01-01", trim_end="2010-12-13"):
+        self.OIS = 0.01 * quandl.get("USTREASURY/YIELD", authtoken=QUANDL_API_KEY, trim_start=trim_start,
                                      trim_end=trim_end)
         self.OIS.reset_index(level=0, inplace=True)
         self.datesAll = pd.DataFrame(pd.date_range(trim_start, trim_end), columns=['DATE'])
@@ -113,6 +164,6 @@ class OIS(object):
 
     def getOIS(self, datelist=[]):
         if (len(datelist) != 0):
-            return self.OIS.iloc[datelist]
+            return self.OIS.loc[datelist]
         else:
             return self.OIS
--- UnitTests/test_corporateRates.py
+++ UnitTests/test_corporateRates.py
@@ -14,16 +14,20 @@ t_step = 1.0 / 365.0
 simNumber = 10
 start = date(2005, 3, 30)
 trim_start = date(2000, 1, 1)
-trim_end = date(2010, 12, 31)
+trim_end = date(2000, 12, 31)
 referenceDate = date(2005, 3, 30)
+
 xR = [2.0, 0.05, 0.01, 0.07]
 
 # CashFlow Dates
+testSched = pd.date_range(trim_start,trim_start)
+scheduleComplete = pd.date_range(start=trim_start,end=trim_end)
 myCorp = CorporateRates()
-myCorp.getCorporates(trim_start, trim_end)
+myCorp.getCorporatesFred(trim_start,trim_end)
+testRatings = myCorp.getCorporateData("AAA",testSched)
+testSurvival = myCorp.getCorporateQData("AAA",scheduleComplete,0.4)
 myCorp.pickleMe()
 
-scheduleComplete = pd.date_range(start=trim_start,end=trim_end)
 
 
 class TestCorporateRates(TestCase):
